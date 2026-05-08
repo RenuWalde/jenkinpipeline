0fff87da0fd413b28e90f68bad4dcf6065336161
RenuWalde <renuwalde@gmail.com>
2026-05-08 07:56:07 +0530

    display

---
+++ src/test/java/jenkin/jenkingjava1.java
@@ -0,0 +1,14 @@
+package jenkin;
+
+public class jenkingjava1 {
+	public void display() {
+		System.err.println("hello world");
+	}
+
+	public static void main(String[] args) {
+		
+		jenkingjava1 l=new jenkingjava1();
+		l.display();
+	}
+
+}
