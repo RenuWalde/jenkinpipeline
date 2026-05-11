d41567e314aec0a4f92260e9fd7841f9511d37e4
RenuWalde <renuwalde@gmail.com>
2026-05-11 07:53:02 +0530

    Update jenkingjava1.java

--- src/test/java/jenkin/jenkingjava1.java
+++ src/test/java/jenkin/jenkingjava1.java
@@ -2,7 +2,7 @@ package jenkin;
 
 public class jenkingjava1 {
 	public void display() {
-		System.err.println("hello world");
+		System.err.println("hello world")
 	}
 
 	public static void main(String[] args) {
